2733759627b3f7bbb50c48bd6f18a967cda5acbd
seula <tmfdk0213@gmail.com>
2023-06-07 22:26:12 +0900

    button components수정

--- src/pages/FeedList.jsx
+++ src/pages/FeedList.jsx
@@ -1,5 +1,30 @@
+import { LargeButton, LargeButtonDisabled } from 'components/Common/Button/Large/LargeButton'
+import { MsmallButton, MsmallButtonDisabled, MsmallWhiteButton } from 'components/Common/Button/Msmall/MsmallButton'
+import {
+  MediumButton,
+  MediumButtonDisabled,
+  MediumDarkButton,
+  MediumWhiteButton,
+} from 'components/Common/Button/Medium/MediumButton'
+import { SmallButton, SmallButtonDisabled, SmallWhiteButton } from 'components/Common/Button/Small/SmallButton'
+
 const FeedList = () => {
-  return <div></div>
+  return (
+    <div>
+      <LargeButton />
+      <LargeButtonDisabled />
+      <MediumButton />
+      <MediumButtonDisabled />
+      <MediumDarkButton />
+      <MediumWhiteButton />
+      <MsmallButton />
+      <MsmallButtonDisabled />
+      <MsmallWhiteButton />
+      <SmallButton />
+      <SmallButtonDisabled />
+      <SmallWhiteButton />
+    </div>
+  )
 }
 
 export default FeedList
