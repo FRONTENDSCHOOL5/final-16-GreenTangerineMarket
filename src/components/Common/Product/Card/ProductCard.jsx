8ea65415ac7abada0fc5130e1a4b2e810a1acae8
ho-ji <wkdd0205@gmail.com>
2023-06-09 15:58:05 +0900

    feat: Product Card 컴포넌트 추가
    Product Card 컴포넌트를 추가함

---
+++ src/components/Common/Product/Card/ProductCard.jsx
@@ -0,0 +1,31 @@
+import { Link } from 'react-router-dom'
+import { useState } from 'react'
+import NoImage from 'assets/img/no-image.png'
+import s from './ProductCard.module.scss'
+import { formatUpdateTime } from 'utils/formatUpdateTime'
+import { formatNumberWithComma } from 'utils/formatNumberWithComma'
+
+const ProductCard = ({ id, image, name, price, time }) => {
+  price = formatNumberWithComma(price)
+  const updateTime = formatUpdateTime(time)
+  const [imageError, setImageError] = useState(false)
+
+  const handleError = e => {
+    setImageError(true)
+  }
+  return (
+    <article className={s.card}>
+      <Link to={`/product/detail/${id}`} className={s.link}>
+        {<img onError={handleError} src={!imageError ? image : NoImage} alt='상품이미지' className={s.image} />}
+        <p className={s.name}>{name}</p>
+        <div className={s.wrap}>
+          <span className={s.price}>{price}</span>
+          <span className={s.won}>원</span>
+        </div>
+      </Link>
+      <p className={s.time}>{updateTime}</p>
+    </article>
+  )
+}
+
+export default ProductCard
