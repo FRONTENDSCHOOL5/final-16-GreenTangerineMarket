f5341e438a70e6e1b86ae6c6985158e29f376e0a
Daniel <wswm2007@naver.com>
2023-06-12 17:01:21 +0900

    design: footer className 변경

--- src/components/Common/Footer/Footer.jsx
+++ src/components/Common/Footer/Footer.jsx
@@ -6,8 +6,8 @@ import yt from 'assets/img/icon-yt.svg'
 const Footer = () => {
   return (
     <footer className={s.footer}>
-      <div className={s.footerCase}>
-        <ul className={s.footerLink}>
+      <div className={s.case}>
+        <ul className={s.link}>
           <li>청귤마켓 소개</li>
           <li>이용약관</li>
           <li>개인정보처리방침</li>
@@ -15,7 +15,7 @@ const Footer = () => {
           <li>청소년보호정책</li>
           <li className={s.lastText}>제휴문의</li>
         </ul>
-        <ul className={s.footerSns}>
+        <ul className={s.sns}>
           <li>
             <a target='_blank'>
               <img src={insta} alt='인스타그램으로 이동하는 회색 인스타그램 로고입니다.' />
@@ -34,7 +34,7 @@ const Footer = () => {
         </ul>
       </div>
 
-      <div className={s.footerText}>
+      <div className={s.text}>
         (주)HODU SHOP <br />
         <address>제주특별자치도 제주시 동광고 137 제주코딩베이스캠프 </address>
         사업자 번호 : 000-0000-0000 | 통신판매업 <br />
