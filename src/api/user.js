55e8772896a6e66a9ca0fb4206ca79d168cc02c4
Limttugi <junhuk820@gmail.com>
2023-06-12 17:40:40 +0900

    refactor: API 에러핸들링 api 파일 내부에 추가

--- src/api/user.js
+++ src/api/user.js
@@ -2,13 +2,21 @@ import { instance } from 'api'
 
 /** 회원가입 API */
 export const signUpAPI = async ({ username, email, password, accountname, intro, image }) => {
-  return await instance.post('/user', { user: { username, email, password, accountname, intro, image } })
+  try {
+    const res = await instance.post('/user', { user: { username, email, password, accountname, intro, image } })
+    console.log(res)
+    return res
+  } catch (err) {
+    console.error(err)
+    return err
+  }
 }
 
 /** 로그인 API */
-export const loginAPI = async (email, password) => {
+export const loginAPI = async ({ email, password }) => {
   try {
     const res = await instance.post('/user/login', { user: { email, password } })
+    console.log(res)
     return res
   } catch (err) {
     console.error(err)
@@ -17,11 +25,25 @@ export const loginAPI = async (email, password) => {
 }
 
 /** 이메일 검증 API */
-export const verifyEmailAPI = async email => {
-  return await instance.post('/user/emailvalid', { user: { email } })
+export const verifyEmailAPI = async ({ email }) => {
+  try {
+    const res = await instance.post('/user/emailvalid', { user: { email } })
+    console.log(res)
+    return res
+  } catch (err) {
+    console.error(err)
+    return err
+  }
 }
 
 /** 계정 검증 API */
-export const verifyAccountAPI = async accountname => {
-  return await instance.post('/user/accountnamevalid', { user: { accountname } })
+export const verifyAccountNameAPI = async ({ accountname }) => {
+  try {
+    const res = await instance.post('/user/accountnamevalid', { user: { accountname } })
+    console.log(res)
+    return res
+  } catch (err) {
+    console.error(err)
+    return err
+  }
 }
