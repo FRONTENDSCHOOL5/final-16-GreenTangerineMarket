1dcc4c0e9d5670de1839fd5671671d9941551c85
ho-ji <wkdd0205@gmail.com>
2023-06-14 13:53:22 +0900

    feat: 더보기 버튼 UI작업 및 클릭 이벤트 추가
    더보기 버튼 선택 시 하단에 드롭메뉴가 나오고 해당영역 제외하고 클릭 시 메뉴가 닫히는 기능 추가

--- src/components/Common/Feed/MoreButton/FeedMoreButton.jsx
+++ src/components/Common/Feed/MoreButton/FeedMoreButton.jsx
@@ -1,11 +1,51 @@
+import { useRecoilValue } from 'recoil'
 import s from './FeedMoreButton.module.scss'
+import { userInfoAtom } from 'recoil/atom/user'
+import { useEffect, useRef, useState } from 'react'
 
-const FeedMoreButton = () => {
-  const showMenu = () => {}
+const FeedMoreButton = ({ id, author }) => {
+  const [showMenu, setShowMenu] = useState(false)
+  const menuRef = useRef(null)
+  const moreButtonRef = useRef(null)
+  const userInfo = useRecoilValue(userInfoAtom)
+  const isMyFeed = author._id === userInfo._id
+  useEffect(() => {
+    const handleOutsideClick = e => {
+      console.log(e, menuRef, moreButtonRef)
+      if (!menuRef.current && moreButtonRef.current.contains(e.target)) {
+        setShowMenu(true)
+      } else if (showMenu && menuRef.current && !menuRef.current.contains(e.target)) {
+        setShowMenu(false)
+      }
+    }
+    document.addEventListener('click', handleOutsideClick)
+    return () => {
+      document.removeEventListener('click', handleOutsideClick)
+    }
+  })
   return (
-    <button type='button' onClick={showMenu} className={s.more}>
-      <span className='a11y-hidden'>더보기 버튼</span>
-    </button>
+    <>
+      <button type='button' className={s.more} ref={moreButtonRef}>
+        <span className='a11y-hidden'>더보기 버튼</span>
+      </button>
+      {showMenu &&
+        (isMyFeed ? (
+          <ul className={s.menu} ref={menuRef}>
+            <li>
+              <button type='button'>수정</button>
+            </li>
+            <li>
+              <button type='button'>삭제</button>
+            </li>
+          </ul>
+        ) : (
+          <ul className={s.menu} ref={menuRef}>
+            <li>
+              <button type='button'>신고</button>
+            </li>
+          </ul>
+        ))}
+    </>
   )
 }
 
