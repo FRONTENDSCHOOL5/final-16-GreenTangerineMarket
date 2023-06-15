a8ce49d175f3c68525e422d956d5d527da8db1d1
seula <tmfdk0213@gmail.com>
2023-06-15 10:44:02 +0900

    design: NotFoundPage 수정

--- src/components/NotFound/NotFoundPage.jsx
+++ src/components/NotFound/NotFoundPage.jsx
@@ -5,14 +5,14 @@ import { useNavigate } from 'react-router-dom/dist'
 
 const NotFoundPage = () => {
   const navigate = useNavigate()
-  const mainHomeClick = () => {
+  const clickMainHome = () => {
     navigate('/')
   }
-  const backPageClick = () => {
+  const clickBackPage = () => {
     navigate(-1)
   }
   return (
-    <main className={s.NotFoundWrapper}>
+    <main className={s.notFoundWrapper}>
       <img className={s.notFoundImg} src={notFoundImg} alt='페이지를 찾을 수 없습니다' />
       <section className={s.notFoundText}>
         <h1 className={s.notFoundHeader}>페이지를 찾을 수 없습니다</h1>
@@ -22,8 +22,8 @@ const NotFoundPage = () => {
           웹주소가 올바른지 확인하세요
         </p>
         <div className={s.notFoundMainButton}>
-          <LargeButton onClickEvent={mainHomeClick}>메인으로</LargeButton>
-          <LargeButtonDisabled onClickEvent={backPageClick}>이전페이지</LargeButtonDisabled>
+          <LargeButton onClickEvent={clickMainHome}>메인으로</LargeButton>
+          <LargeButtonDisabled onClickEvent={clickBackPage}>이전페이지</LargeButtonDisabled>
         </div>
       </section>
     </main>
