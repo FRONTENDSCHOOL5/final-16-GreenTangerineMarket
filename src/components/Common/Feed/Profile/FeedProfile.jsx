50ed869316141f453322118d3235498a4460c7bf
ho-ji <wkdd0205@gmail.com>
2023-06-16 23:31:27 +0900

    refactor: 프로필 이미지 컴포넌트 분리
    프로필이미지 에러 처리 재사용으로 인한 컴포넌트 분리

--- src/components/Common/Feed/Profile/FeedProfile.jsx
+++ src/components/Common/Feed/Profile/FeedProfile.jsx
@@ -1,22 +1,13 @@
 import { Link } from 'react-router-dom'
+
 import s from './FeedProfile.module.scss'
-import defaultProfile from 'assets/img/default-profile.svg'
-import { useState } from 'react'
+
+import ProfileImage from '../ProfileImage/ProfileImage'
 
 const FeedProfile = ({ author }) => {
-  const [profileImageError, setProfileImageError] = useState(false)
-
-  const handleProfileImageError = e => {
-    setProfileImageError(true)
-  }
   return (
     <Link to={`/profile/${author.accountname}`} className={s.profile}>
-      <img
-        src={author.image && !profileImageError ? author.image : defaultProfile}
-        onError={handleProfileImageError}
-        alt={`${author.accountname} 프로필 이미지`}
-        className={s.profileImg}
-      />
+      <ProfileImage image={author.image} name={author.accountname} className={s.image} />
       <div className={s.author}>
         <p className={s.user}>{author.username}</p>
         <p className={s.account}>@{author.accountname}</p>
