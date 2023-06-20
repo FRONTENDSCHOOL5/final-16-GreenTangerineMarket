5a0c3b9be441af96a1dba801f3ec25932ca06f6a
Limttugi <junhuk820@gmail.com>
2023-06-20 17:45:24 +0900

    refactor: 이미지 업로드 버튼 클릭시에 실행 및 회원가입 버튼 여러번 못누르게 코드 리팩토링

--- src/components/Common/InputBox/ProfileImageInputBox/ProfileImageInputBox.jsx
+++ src/components/Common/InputBox/ProfileImageInputBox/ProfileImageInputBox.jsx
@@ -2,30 +2,24 @@ import { useState } from 'react'
 
 import s from './ProfileImageInputBox.module.scss'
 
-import { uploadImage } from 'api/image'
-import { PROFILE_IMAGE_ONLY_UPLOAD_IMAGE } from 'constants/SIGN_ERROR'
 import basicProfileImg from 'assets/img/basic-profile-img.svg'
 import uploadImg from 'assets/img/upload.svg'
 
-const ProfileImageInputBox = ({ initialImage = basicProfileImg, setImage }) => {
+const ProfileImageInputBox = ({ initialImage = basicProfileImg }) => {
   const [profileImage, setProfileImage] = useState(initialImage)
 
-  const handleProfileImageUpload = async e => {
+  const handleSetProfileImage = e => {
+    const reader = new FileReader()
     const file = e.target.files[0]
-    const formData = new FormData()
-    formData.append('image', file)
-    const res = await uploadImage(formData)
 
-    if (res.data.message === PROFILE_IMAGE_ONLY_UPLOAD_IMAGE) {
-      alert(PROFILE_IMAGE_ONLY_UPLOAD_IMAGE)
-    } else {
-      setProfileImage(`https://api.mandarin.weniv.co.kr/${res.data.filename}`)
-      setImage(`https://api.mandarin.weniv.co.kr/${res.data.filename}`)
+    reader.onload = () => {
+      setProfileImage(reader.result)
     }
+    reader.readAsDataURL(file)
   }
 
   return (
-    <label className={s.profileImage} onChange={handleProfileImageUpload}>
+    <label className={s.profileImage} onChange={handleSetProfileImage}>
       <img src={profileImage} alt='프로필 이미지 선택' className={s.basicProfileImg} />
       <img src={uploadImg} alt='프로필 이미지 선택' className={s.uploadImg} />
       <input type='file' name='image' accept='image/jpg, image/gif, image/png, image/bmp, image/tif, image/heic' />
