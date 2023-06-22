7630894863a7f02b0cfd2b20c28a92bafa3595c9
ho-ji <wkdd0205@gmail.com>
2023-06-22 15:52:00 +0900

    refactor: button hover, 폴더명 오류, noitem 레이아웃 수정
     프로필페이지 탭메뉴 button hover 추가, 폴더명 오류수정 , noitem에서 gap대신 marign-top으로 레이아웃 수정

--- src/components/Profile/NoItem/ProfileNoItem.jsx
+++ src/components/Profile/NoItem/ProfileNoItem.jsx
@@ -16,13 +16,13 @@ const ProfileNoItem = ({ item, action = '' }) => {
       {action ? (
         <button type='button' onClick={handleAddItemClick} className={s.info}>
           <img src={imageAddItem} alt='청귤' />
-          <span>등록된 {item === 'feed' ? '피드가' : '상품이'} 없습니다</span>
+          <span className={s.text}>등록된 {item === 'feed' ? '피드가' : '상품이'} 없습니다</span>
           <span className={s.add}>첫 {item === 'feed' ? '피드' : '상품'} 등록하기</span>
         </button>
       ) : (
         <div className={s.info}>
           <img src={imageNoItem} alt='청귤' />
-          <p>등록된 {item === 'feed' ? '피드가' : '상품이'} 없습니다</p>
+          <p className={s.text}>등록된 {item === 'feed' ? '피드가' : '상품이'} 없습니다</p>
         </div>
       )}
     </div>
