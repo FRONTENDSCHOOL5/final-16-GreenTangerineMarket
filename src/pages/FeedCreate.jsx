c26d08a6feecb19630a3e0af6308fe78f6f839b2
Daniel <wswm2007@naver.com>
2023-06-23 00:08:12 +0900

    remove: productCreate 및 feedCreate 폴더 컴퍼넌트로 이동

--- src/pages/FeedCreate.jsx
+++ src/pages/FeedCreate.jsx
@@ -1,6 +1,6 @@
 import Footer from 'components/Common/Footer/Footer'
 import Header from 'components/Common/Header/Header'
-import FeedCreateComponent from 'components/FeedList/FeedCreate/FeedCreateComponent'
+import FeedCreateComponent from 'components/FeedCreate/FeedCreateComponent'
 
 const FeedCreate = () => {
   return (
--- src/pages/ProductCreate.jsx
+++ src/pages/ProductCreate.jsx
@@ -1,6 +1,6 @@
 import Footer from 'components/Common/Footer/Footer'
 import Header from 'components/Common/Header/Header'
-import ProductCreateComponent from 'components/Common/Product/ProductCreate/ProductCreateComponent'
+import ProductCreateComponent from 'components/ProductCreate/ProductCreateComponent'
 
 const ProuctCreate = () => {
   return (
