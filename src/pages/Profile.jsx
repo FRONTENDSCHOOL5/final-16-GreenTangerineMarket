3a30a32a08a7b0e0e49c27b71511f027b9edc44f
ho-ji <wkdd0205@gmail.com>
2023-06-23 20:40:37 +0900

    feat: 프로필페이지 간 이동 시, 스크롤은 무조건 상단에 오도록 추가
    프로필페이지 간에 이동 발생 시, 스크롤은 항상 최상단으로 이동하도록 추가함

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -3,9 +3,14 @@ import { useParams } from 'react-router'
 import ProfileInfo from 'components/Profile/Info/ProfileInfo'
 import ProfileItemList from 'components/Profile/List/ItemList/ProfileItemList'
 import ListLayout from 'components/Common/Layout/List/ListLayout'
+import { useEffect } from 'react'
 
 const Profile = () => {
   const { accountname } = useParams()
+
+  useEffect(() => {
+    window.scrollTo({ top: 0 })
+  }, [accountname])
   return (
     <ListLayout name=''>
       <ProfileInfo accountname={accountname} />
