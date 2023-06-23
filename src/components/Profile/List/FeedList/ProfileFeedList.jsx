2538711add6c81ebcc143fff42fefdb54c500d25
ho-ji <wkdd0205@gmail.com>
2023-06-23 16:04:06 +0900

    fix: 무한스크롤 시, 프로필 데이터 업데이트 오류 수정
    프로필 페이지에서 프로필 페이지 이동 시 프로필 데이터 업데이트가 잘되지 않던 오류 수정

--- src/components/Profile/List/FeedList/ProfileFeedList.jsx
+++ src/components/Profile/List/FeedList/ProfileFeedList.jsx
@@ -15,14 +15,18 @@ const ProfileFeedList = ({ accountname }) => {
   const [isMoreData, setIsMoreData] = useState(true)
 
   const loadUserFeeds = async page => {
+    console.log(userFeeds)
     if (isMoreData) {
       const res = await getUserFeedList({ num: page * 10, accountname: accountname })
       if (res.status === 200) {
-        !res.data.post.length
-          ? setIsMoreData(false)
-          : page === 0
-          ? setUserFeeds([...res.data.post])
-          : setUserFeeds([...userFeeds, ...res.data.post])
+        if (res.data.post.length === 0) setIsMoreData(false)
+        else {
+          if (page === 0) {
+            setUserFeeds(prevUserFeeds => [...res.data.post])
+          } else {
+            setUserFeeds(prevUserFeeds => [...prevUserFeeds, ...res.data.post])
+          }
+        }
       }
     }
   }
