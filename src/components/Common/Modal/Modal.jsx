3482e65d36a793e6ab692cbe903c19cfaff5e28a
ho-ji <wkdd0205@gmail.com>
2023-06-23 11:13:39 +0900

    feat: 모달컴포넌트 분리 및 모달창이 떠있을때 스크롤 방지
    모달 컴포넌트를 분리하고 모달창이 떠있을때 뒤에 스크롤이 안되도록 추가함

---
+++ src/components/Common/Modal/Modal.jsx
@@ -0,0 +1,50 @@
+import { Children, useEffect } from 'react'
+import s from './Modal.module.scss'
+
+const Modal = ({ children, closeModal }) => {
+  const handleOutsideClick = e => {
+    if (e.target.nodeName === 'DIALOG') closeModal()
+  }
+
+  const preventScroll = () => {
+    const currentScrollY = window.scrollY
+    document.body.style.position = 'fixed'
+    document.body.style.width = '100%'
+    document.body.style.top = `-${currentScrollY}px`
+    document.body.style.overflowY = 'scroll'
+    return currentScrollY
+  }
+
+  const allowScroll = prevScroll => {
+    document.body.style.position = ''
+    document.body.style.width = ''
+    document.body.style.top = ''
+    document.body.style.overflowY = ''
+    window.scrollTo(0, prevScroll)
+  }
+
+  useEffect(() => {
+    const handleEscapeKeyDown = e => {
+      if (e.key === 'Escape') {
+        closeModal()
+        e.target.blur()
+      }
+    }
+    const prevScroll = preventScroll()
+    document.addEventListener('keydown', handleEscapeKeyDown)
+    return () => {
+      document.removeEventListener('keydown', handleEscapeKeyDown)
+      document.body.style.overflow = ''
+
+      allowScroll(prevScroll)
+    }
+  })
+
+  return (
+    <dialog className={s.modal} onClick={handleOutsideClick}>
+      <div className={s.container}>{children}</div>
+    </dialog>
+  )
+}
+
+export default Modal
