2d0b2ea2f96c26383d85c6de0482a48eced3fd24
ho-ji <wkdd0205@gmail.com>
2023-06-23 20:44:17 +0900

    refactor: import문 정리
    import문 정리함

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,9 +1,9 @@
-import { useParams } from 'react-router'
+import { useParams } from 'react-router-dom'
+import { useEffect } from 'react'
 
 import ProfileInfo from 'components/Profile/Info/ProfileInfo'
 import ProfileItemList from 'components/Profile/List/ItemList/ProfileItemList'
 import ListLayout from 'components/Common/Layout/List/ListLayout'
-import { useEffect } from 'react'
 
 const Profile = () => {
   const { accountname } = useParams()
