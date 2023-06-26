798474a90220a2d219bf5daf27959af15820826c
ho-ji <wkdd0205@gmail.com>
2023-06-26 10:14:22 +0900

    feat: 검색페이지 추가
    검색페이지 추가

---
+++ src/components/Search/UserListItem/SearchUserListItem.jsx
@@ -0,0 +1,55 @@
+import { useState } from 'react'
+import { toast } from 'react-hot-toast'
+
+import s from './SearchUserListItem.module.scss'
+
+import { Link } from 'react-router-dom'
+import { SmallButton, SmallWhiteButton } from 'components/Common/Button/Small/SmallButton'
+import getToastStyle from 'utils/getToastStyle'
+import { followProfileAPI, unfollowProfileAPI } from 'api/profile'
+import ProfileImage from 'components/Common/ProfileImage/ProfileImage'
+
+const SearchUserListItem = ({ image, accountname, username, follow }) => {
+  const [isFollow, setIsFollow] = useState(follow)
+
+  const handleFollowClick = async e => {
+    e.preventDefault()
+    const res = await followProfileAPI(accountname)
+    if (res.status === 200) {
+      toast('팔로우했습니다', {
+        style: getToastStyle(),
+      })
+      setIsFollow(true)
+    }
+  }
+
+  const handleUnfollowClick = async e => {
+    e.preventDefault()
+    const res = await unfollowProfileAPI(accountname)
+    if (res.status === 200) {
+      toast('팔로우취소했습니다', {
+        style: getToastStyle(),
+      })
+      setIsFollow(false)
+    }
+  }
+
+  return (
+    <li>
+      <Link to={`/profile/${accountname}`} className={s.item}>
+        <ProfileImage image={image} className={s.image} />
+        <div className={s.name}>
+          <p className={s.user}>{username}</p>
+          <p className={s.account}>@{accountname}</p>
+        </div>
+        {isFollow ? (
+          <SmallWhiteButton onClickEvent={handleUnfollowClick}>언팔로우</SmallWhiteButton>
+        ) : (
+          <SmallButton onClickEvent={handleFollowClick}>팔로우</SmallButton>
+        )}
+      </Link>
+    </li>
+  )
+}
+
+export default SearchUserListItem
