9392412dfa7ba2a4c286978825621843d962a02a
ho-ji <wkdd0205@gmail.com>
2023-06-28 15:41:30 +0900

    refactor: 수정버튼 선택시 링크 수정
    수정버튼 선택 시 링크 수정

--- src/components/Common/Feed/EditButton/FeedEditButton.jsx
+++ src/components/Common/Feed/EditButton/FeedEditButton.jsx
@@ -1,10 +1,14 @@
-import { useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router'
+import { useSetRecoilState } from 'recoil'
+import { showEditModalAtom } from 'recoil/atom/showFlag'
 
 const FeedEditButton = ({ id }) => {
+  const setShowEditModal = useSetRecoilState(showEditModalAtom)
   const navigate = useNavigate()
 
   const handleClick = () => {
-    navigate(`/post/edit/${id}`)
+    setShowEditModal(true)
+    navigate(`/feed/detail/${id}`)
   }
 
   return <button onClick={handleClick}>수정</button>
