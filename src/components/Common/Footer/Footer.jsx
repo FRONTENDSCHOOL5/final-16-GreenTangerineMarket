f40883b5559e30461218ab4fe64973f161612095
Daniel <wswm2007@naver.com>
2023-06-30 23:24:29 +0900

    design: footer 전체적인 UI변경을 위한 기초 마련

--- src/components/Common/Footer/Footer.jsx
+++ src/components/Common/Footer/Footer.jsx
@@ -1,45 +1,25 @@
 import s from './Footer.module.scss'
-import insta from 'assets/img/icon-insta.svg'
-import fb from 'assets/img/icon-fb.svg'
-import yt from 'assets/img/icon-yt.svg'
+
+import logoImg from 'assets/img/logo_char.svg'
+import gitHub from 'assets/img/icon-github.svg'
 
 const Footer = () => {
   return (
     <footer className={s.footer}>
-      <div className={s.case}>
-        <ul className={s.link}>
-          <li>청귤마켓 소개</li>
-          <li>이용약관</li>
-          <li>개인정보처리방침</li>
-          <li>전자금융거래약관</li>
-          <li>청소년보호정책</li>
-          <li className={s.lastText}>제휴문의</li>
-        </ul>
-        <ul className={s.sns}>
-          <li>
-            <a target='_blank'>
-              <img src={insta} alt='인스타그램으로 이동하는 회색 인스타그램 로고입니다.' />
-            </a>
-          </li>
-          <li>
-            <a target='_blank'>
-              <img src={fb} alt='페이스북으로 이동하는 회색 페이스북 로고입니다.' />
-            </a>
-          </li>
-          <li>
-            <a target='_blank'>
-              <img src={yt} alt='유튜브로 이동하는 회색 유튜브 로고입니다.' />
-            </a>
-          </li>
-        </ul>
+      <div>
+        <img src={logoImg} alt='' />
       </div>
 
       <div className={s.text}>
-        (주)HODU SHOP <br />
-        <address>제주특별자치도 제주시 동광고 137 제주코딩베이스캠프 </address>
-        사업자 번호 : 000-0000-0000 | 통신판매업 <br />
-        대표 : 김호두
+        본 페이지는 상업적인 목적이 없는 포트폴리오용 사이트입니다
+        <br />
+        장예지, 임준혁, 이슬아, 정승규 copyright
+        <button className={s.contact}>contect us</button>
       </div>
+
+      <button className={s.gitHub}>
+        <img src={gitHub} alt='' />
+      </button>
     </footer>
   )
 }
