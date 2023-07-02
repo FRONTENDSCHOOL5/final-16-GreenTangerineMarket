0eb7f44625b48c8aa7f1e209f9cb60fd98497517
ho-ji <wkdd0205@gmail.com>
2023-07-02 23:08:32 +0900

    refactor: 신고여부 모달 제거
    신고여부 확인 모달 제거

--- src/components/Common/Feed/ReportButton/FeedReportButton.jsx
+++ src/components/Common/Feed/ReportButton/FeedReportButton.jsx
@@ -1,25 +1,9 @@
-import { useState } from 'react'
 import { toast } from 'react-hot-toast'
 
-import s from './FeedReportButton.module.scss'
-
-import { MsmallButton, MsmallWhiteButton } from 'components/Common/Button/Msmall/MsmallButton'
 import { reportFeedAPI } from 'api/feed'
 import getToastStyle from 'utils/getToastStyle'
-import Modal from 'components/Common/Modal/Modal'
 
 const FeedReportButton = ({ id, closeMenu }) => {
-  const [isModalOpen, setIsModalOpen] = useState(false)
-
-  const handleReportClick = () => setIsModalOpen(true)
-
-  const closeModal = () => setIsModalOpen(false)
-
-  const cancelReport = () => {
-    closeModal()
-    closeMenu()
-  }
-
   const reportFeed = async () => {
     const res = await reportFeedAPI(id)
     if (res.status === 200) {
@@ -27,29 +11,13 @@ const FeedReportButton = ({ id, closeMenu }) => {
         style: getToastStyle(),
       })
     }
-    closeModal()
     closeMenu()
   }
 
   return (
-    <>
-      <button type='button' onClick={handleReportClick}>
-        신고
-      </button>
-      {isModalOpen && (
-        <Modal closeModal={closeModal}>
-          <div className={s.container}>
-            <p className={s.announce}>
-              해당 피드를 <strong>신고</strong>하시겠습니까?
-            </p>
-            <div className={s.button}>
-              <MsmallWhiteButton onClickEvent={cancelReport}>취소</MsmallWhiteButton>
-              <MsmallButton onClickEvent={reportFeed}>확인</MsmallButton>
-            </div>
-          </div>
-        </Modal>
-      )}
-    </>
+    <button type='button' onClick={reportFeed}>
+      신고
+    </button>
   )
 }
 
