4d9ef84c3ca7e220a97efa58ea5fd9edf7f1bc73
ho-ji <wkdd0205@gmail.com>
2023-07-12 23:39:41 +0900

    refactor: FeedDetailHeader 이름 변경 및, 팔로우버튼/수정 삭제 버튼 추가
    header-> div로 태그 변경하면서 FeedDetailAuthor로 이름 변경하고, 상단에 본인의 피드일 경우엔 수정/ 삭제 버튼을, 타인의 피드일 경우엔 팔로우 버튼이 노출되도록 수정함

---
+++ src/components/FeedDetail/Author/FeedDetailAuthor.jsx
@@ -0,0 +1,68 @@
+import { Link } from 'react-router-dom'
+import s from './FeedDetailAuthor.module.scss'
+
+import ProfileImage from 'components/Common/ProfileImage/ProfileImage'
+import AuthorButtonList from '../AuthorButtonList/AuthorButtonList'
+import { useRecoilValue } from 'recoil'
+import { myInfoAtom } from 'recoil/atom/user'
+import { useState } from 'react'
+import { followProfileAPI, unfollowProfileAPI } from 'api/profile'
+import { toast } from 'react-hot-toast'
+import getToastStyle from 'utils/getToastStyle'
+
+const FeedDetailAuthor = ({ feedDetail }) => {
+  const [isFollow, setIsFollow] = useState(feedDetail.author.isfollow)
+  const myInfo = useRecoilValue(myInfoAtom)
+
+  const handleUnfollow = async () => {
+    if (isFollow) {
+      setIsFollow(false)
+      const res = await unfollowProfileAPI(feedDetail.author.accountname)
+      if (res.status === 200) {
+        toast('팔로우취소했습니다', {
+          style: getToastStyle(),
+        })
+      }
+    }
+  }
+  const handleFollow = async () => {
+    if (!isFollow) {
+      setIsFollow(true)
+      const res = await followProfileAPI(feedDetail.author.accountname)
+      if (res.status === 200) {
+        toast('팔로우했습니다', {
+          style: getToastStyle(),
+        })
+      }
+    }
+  }
+
+  return (
+    <>
+      {feedDetail && (
+        <div className={s.container}>
+          <Link to={`/profile/${feedDetail.author.accountname}`} className={s.profile}>
+            <ProfileImage image={feedDetail.author.image} username={feedDetail.author.username} className={s.image} />
+            <div className={s.name}>
+              <p className={s.user}>{feedDetail.author.username}</p>
+              <p className={s.account}>@{feedDetail.author.accountname}</p>
+            </div>
+          </Link>
+          {feedDetail.author.accountname === myInfo.accountname ? (
+            <AuthorButtonList feedDetail={feedDetail} />
+          ) : isFollow ? (
+            <button type='button' onClick={handleUnfollow} className={s.unfollow}>
+              언팔로우
+            </button>
+          ) : (
+            <button type='button' onClick={handleFollow} className={s.follow}>
+              팔로우
+            </button>
+          )}
+        </div>
+      )}
+    </>
+  )
+}
+
+export default FeedDetailAuthor
