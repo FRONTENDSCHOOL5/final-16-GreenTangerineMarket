53942b5dd4b67d21626aa0291b84ab1d1a9a54d8
ho-ji <wkdd0205@gmail.com>
2023-07-15 23:32:26 +0900

    refactor: imageSlider 에러 메세지 수정
    이미지 슬라이더에서 currentSlide와 slideCount에러메시지 뜨는 것 수정함

--- src/components/Common/Slider/ImageSlider.jsx
+++ src/components/Common/Slider/ImageSlider.jsx
@@ -4,9 +4,13 @@ import s from './ImageSlider.module.scss'
 import 'slick-carousel/slick/slick.css'
 import 'slick-carousel/slick/slick-theme.css'
 
-const CustomPrevArrow = props => <button {...props} className={s.prev} type='button'></button>
+const CustomPrevArrow = ({ currentSlide, slideCount, ...props }) => (
+  <button {...props} className={s.prev} type='button'></button>
+)
 
-const CustomNextArrow = props => <button {...props} className={s.next} type='button'></button>
+const CustomNextArrow = ({ currentSlide, slideCount, ...props }) => (
+  <button {...props} className={s.next} type='button'></button>
+)
 
 const ImageSlider = ({ imageType, children }) => {
   const settings = {
