245167b5aaa1ed711e9e784928eb90e4944325a7
ho-ji <wkdd0205@gmail.com>
2023-07-16 21:44:03 +0900

    feat: 피드 상세페이지에서 이미지 에러 핸들링
    피드 상세페이지에서 이미지 에러 핸들링

--- src/components/Common/List/ImageList.jsx
+++ src/components/Common/List/ImageList.jsx
@@ -1,9 +1,15 @@
+import { useState } from 'react'
+
 import s from './ImageList.module.scss'
 
+import noImage from 'assets/img/no-image.png'
+
 const ImageList = ({ src, alt }) => {
+  const [imageError, setImageError] = useState(false)
+
   return (
     <li className={s.list}>
-      <img className={s.img} src={src} alt={alt} />
+      <img className={s.img} src={!imageError ? src : noImage} alt={alt} onError={() => setImageError(true)} />
     </li>
   )
 }
